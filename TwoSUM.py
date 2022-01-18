f67f55abf9e8c30366d4fa92994cc4eb3541ca65
Shishir98 <shishir98@live.com>
2022-01-18 21:25:14 +0530

    Update TwoSUM.py
    Hard Breach
    Dictionary 
    Easy Problem

--- TwoSUM.py
+++ TwoSUM.py
@@ -1,7 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        #hardbreach method, 2 for loops
+        #hardbreach method, 2 for loops O(n^2)
         
         def hardBreach(self, nums, target):
             i=0
@@ -11,6 +11,19 @@ class Solution:
                     if nums[i] + nums[j] ==  target:
                         return(i,j)
         return hardBreach(self,nums,target)
+    
+    class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        #Dictionary Method O(n)
+        
+        def dictMethod(self, nums, target):
+            dic = {}
+            for i, n in enumerate(nums): 
+                if n in dic:
+                    return [dic[n], i]
+                dic[target-n] = i
+        return dictMethod(self,nums, target)
       
  
                 
